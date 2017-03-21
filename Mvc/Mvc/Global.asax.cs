3ddc3002342b3741c3db345e7818b7588f87ebb2
wangze_c <wangzekw@126.com>
2017-03-21 11:02:09 +0800

    调整项目结构

--- Mvc/Mvc/Global.asax.cs
+++ Mvc/Mvc/Global.asax.cs
@@ -15,7 +15,6 @@ namespace Mvc
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
