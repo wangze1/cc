8300a12cb69a3ca460775a23bfaf1d6af5fc44a3
wangze_c <wangzekw@126.com>
2017-03-28 12:00:05 +0800

    log4 日志应用

---
+++ Test/Log4Test/Program.cs
@@ -0,0 +1,45 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log4Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InitLog4Net();
+
+            var logger = LogManager.GetLogger(typeof(Program));
+
+            // 一般信息
+            logger.Info("消息");
+            // 警告
+            logger.Warn("警告");
+            // 一般错误
+            logger.Error("异常");
+            // 一般错误(格式化)
+            logger.ErrorFormat("异常{0}", "Test");
+            // 调试信息
+            logger.Debug("调试");
+            // 致命错误
+            logger.Fatal("错误");
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// 初始化log4 配置
+        /// </summary>
+        static void InitLog4Net()
+        {
+            FileInfo logCfg = new FileInfo(AppDomain.CurrentDomain.BaseDirectory + "/Log4Config/log4net.config");
+            XmlConfigurator.Configure(logCfg);
+        }
+    }
+}
